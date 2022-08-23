8331a6537de0e39b4871c88da0abf83303271357
Nikolai <nik-neman@mail.ru>
2022-08-23 19:17:41 +0200

    add two fragments

--- app/src/main/java/com/example/myfirstandroidapplication/LoginPageActivity.java
+++ app/src/main/java/com/example/myfirstandroidapplication/LoginPageActivity.java
@@ -1,8 +1,11 @@
 package com.example.myfirstandroidapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginPageActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class LoginPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+    }
+
+    public void FragmentChange(View v){
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        switch (v.getId()){
+            case R.id.fr1:
+                InfoFragment info_Fragment = new InfoFragment();
+                ft.replace(R.id.fragment, info_Fragment);
+                break;
+
+            case R.id.fr2:
+                SecondFragment second_Fragment = new SecondFragment();
+                ft.replace(R.id.fragment, second_Fragment);
+                break;
+
+        }
+        ft.commit();
     }
 }
